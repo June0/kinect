8a93fb3381520d66d6261f1b6576c3380ceb1a23
jbmaigrot <jb.maigrot@neuf.fr>
2017-12-04 16:08:08 +0100

    offset + some fixes

--- TP3_ARVR/main.cpp
+++ TP3_ARVR/main.cpp
@@ -1,6 +1,7 @@
 #include "vrpn_Tracker.h"
 #include <Windows.h>
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 //Global variables
@@ -19,21 +20,21 @@ void VRPN_CALLBACK handle_tracker(void* userData, const vrpn_TRACKERCB t)
 {
 	switch(t.sensor)
 	{
-		case 8: 
+		case 6: //5 6
+			left_elbow_y = t.pos[1];
+			break;
+		case 8: //7 8
 			left_hand_x = t.pos[0];
 			left_hand_y = t.pos[1];
 			left_hand_z = t.pos[2];
 			break;
-		case 6:
-			left_elbow_y = t.pos[1];
+		case 12: //9 12
+			right_elbow_y = t.pos[1];
 			break;
-		case 14:
+		case 14: //11 14
 			right_hand_x = t.pos[0];
 			right_hand_y = t.pos[1];
 			right_hand_z = t.pos[2];
-			break;
-		case 12:
-			right_elbow_y = t.pos[1];
 			break;
 		default: break;
 	}
@@ -56,7 +57,6 @@ void click(bool pressed)
 {
 	POINT p;
 	GetCursorPos(&p);
-	printf("%d %d\n", p.x, p.y);
 
 	INPUT input;
 	input.type = INPUT_MOUSE;
@@ -77,13 +77,31 @@ void click(bool pressed)
 	SendInput(1, &input, sizeof(INPUT));	
 }
 
+void GetDesktopResolution(int& hsize, int& vsize)
+{
+	RECT desktop;
+	// Get a handle to the desktop window
+	const HWND hDesktop = GetDesktopWindow();
+	// Get the size of screen to the variable desktop
+	GetWindowRect(hDesktop, &desktop);
+	// The top left corner will have coordinates (0,0)
+	// and the bottom right corner will have coordinates
+	// (horizontal, vertical)
+	hsize = desktop.right;
+	vsize = desktop.bottom;
+}
+
 
 int main(int argc, char* argv[])
 {
-	char *addr = "Tracker0@172.16.169.2:3883";
+	char *addr = "Tracker0@172.16.169.10:3883";
 
 	float arm_threshold = 0.1;
 	float clap_threshold = 0.1;
+	float h_amplitude = 1.6; //kinect  horizontal amplitude
+	float v_amplitude = 1.2; //kinect  vertical amplitude
+	float h_offset = 0.8;
+	float v_offset = 0.65;
 	bool clap = false;
 	bool left_hand_up = false;
 	bool right_hand_up = false;
@@ -95,7 +113,19 @@ int main(int argc, char* argv[])
 	vrpn_Tracker_Remote *tracker = new vrpn_Tracker_Remote(addr);
 	tracker->register_change_handler(NULL, handle_tracker);
 
+	RECT desktop;
+	// Get a handle to the desktop window
+	const HWND hDesktop = GetDesktopWindow();
+	// Get the size of screen to the variable desktop
+	GetWindowRect(hDesktop, &desktop);
+	int hsize = desktop.right;
+	int vsize = desktop.bottom;
+
 	while (1) {
+		if (GetKeyState(VK_SPACE) & 0x8000)
+		{
+			return 0; //emergency stop
+		}
 		//Handlers
 		tracker->mainloop();
 
@@ -109,6 +139,7 @@ int main(int argc, char* argv[])
 			else {
 				system("start mspaint");
 				paint_is_open = true;
+				click(false);
 			}
 		}
 
@@ -116,20 +147,20 @@ int main(int argc, char* argv[])
 		if (debounce(left_hand_y - left_elbow_y > arm_threshold, &left_hand_up)) {
 			printf("Left arm UP\n");
 		}
-		if (debounce(left_hand_y - left_elbow_y < - arm_threshold, &left_hand_down)) {
+		if (debounce(left_hand_y - left_elbow_y < -arm_threshold, &left_hand_down)) {
 			printf("Left arm DOWN\n");
 		}
 		if (debounce(right_hand_y - right_elbow_y > arm_threshold, &right_hand_up)) {
 			printf("Right arm UP\n");
 		}
-		if (debounce(right_hand_y - right_elbow_y < - arm_threshold, &right_hand_down)) {
+		if (debounce(right_hand_y - right_elbow_y < -arm_threshold, &right_hand_down)) {
 			printf("Right arm DOWN\n");
 		}
 
 		//Paint
 		if (paint_is_open) {
-			//printf("x:%f y:%f z:%f\n", right_hand_x, right_hand_y, right_hand_z);
-			SetCursorPos((right_hand_x + 1) * 500, (-right_hand_y + 1) * 500);
+			SetCursorPos((right_hand_x + h_offset) * hsize / h_amplitude, (-right_hand_y + v_offset) * vsize / v_amplitude);
+			printf("%f %f\n", right_hand_x, right_hand_y);
 			if (left_hand_up) {
 				click(true);
 			}
@@ -137,7 +168,6 @@ int main(int argc, char* argv[])
 				click(false);
 			}
 		}
-
 		Sleep(16);
 	}
 }
